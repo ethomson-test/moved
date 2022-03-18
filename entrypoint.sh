2ccfe90cf192d32a1323e41326304ce1e356d008
Edward Thomson <ethomson@github.com>
2022-03-18 13:39:07 -0400

    test that static data persists between action runs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+set -euo pipefail
+
+# check that a static file on disk has the expected value.
+# this ensures that we are not re-using a container between
+# action invocations.
+
+read STATIC </tmp/static
+test "${STATIC}" = "static_data"
+
+# update a count of a file in the runner workdir
+# this ensures that we can mutate files in the
+# workdir and they are persisted back to the runner
+
 COUNT=0
 
 if [ -f "count" ]; then
